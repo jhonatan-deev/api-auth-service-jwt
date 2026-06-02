627f9acfee8f620b67b00361864b6efd0ccfdb26
jhonatan-deev <jhonatan-deev@gmail.com>
2026-06-02 18:21:37 -0300

    feat: cria projeto Spring Boot 4.0.6 com dependencias iniciais

---
+++ src/test/java/com/jhonatan/authservice/AuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jhonatan.authservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
