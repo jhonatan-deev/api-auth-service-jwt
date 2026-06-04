ff4c810c53615ef8feeec149851d76d5613a782c
jhonatan-deev <jhonatan-deev@gmail.com>
2026-06-04 17:42:36 -0300

    feat: cria AuthService (UserDetailsService) e JwtService (geracao/validacao de tokens)

--- src/main/java/com/jhonatan/authservice/repository/UsuarioRepository.java
+++ src/main/java/com/jhonatan/authservice/repository/UsuarioRepository.java
@@ -3,5 +3,9 @@ package com.jhonatan.authservice.repository;
 import com.jhonatan.authservice.model.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public class UsuarioRepository extends JpaRepository<Usuario, Long> {
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
+    Optional<Usuario> findByLogin(String login);
+    boolean existsByLogin(String login);
 }
